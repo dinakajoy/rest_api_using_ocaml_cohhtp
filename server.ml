c90b969ab8c1aac1cb7689eb87a9a127ea5c4982
dinakajoy <dinakajoy@gmail.com>
2025-05-12 11:15:20 +0100

    updated

--- server.ml
+++ server.ml
@@ -3,41 +3,51 @@ open Printf
 open Cohttp
 open Cohttp_lwt_unix
 
+(* Helper function to load resources from a JSON file *)
 let get_resources source =
   let source_file = source ^ ".json" in
-  let resources = Yojson.Basic.from_file source_file in
-  resources
+  try
+    Yojson.Basic.from_file source_file
+  with
+  | Sys_error msg ->
+      `Assoc [ ("error", `String ("Failed to load resource: " ^ msg)) ]
+  | Yojson.Json_error msg ->
+      `Assoc [ ("error", `String ("Invalid JSON format: " ^ msg)) ]
 
+(* Function to handle API routes *)
+let handle_request uri =
+  match uri with
+  | "" | "/" ->
+      `Assoc [ ("success", `String "Welcome to our test API using OCaml!") ]
+  | "/api/users" | "/api/users/" ->
+      get_resources "users"
+  | "/api/books" | "/api/books/" ->
+      get_resources "books"
+  | _ ->
+      `Assoc [ ("error", `String "Invalid endpoint!") ]
+
+(* Main server logic *)
 let () =
   let on_exn = function
     | Unix.Unix_error (error, func, arg) ->
-        printf "Client connection error %s: %s(%S)" (Unix.error_message error)
-          func arg
-    | exn -> printf "Unhandled exception: %s\n%!" (Printexc.to_string exn)
+        printf "Client connection error %s: %s(%S)\n%!" (Unix.error_message error) func arg
+    | exn ->
+        printf "Unhandled exception: %s\n%!" (Printexc.to_string exn)
   in
+
   let callback _conn req body =
     let uri = req |> Request.resource in
-    let response =
-      match uri with
-      | "" | "/" ->
-          let response =
-            `Assoc
-              [ ("success", `String "Welcome to our test API using OCaml!") ]
-          in
-          Yojson.Basic.pretty_to_string response
-      | "/api/users" -> Yojson.Basic.pretty_to_string (get_resources "users")
-      | "/api/books" -> Yojson.Basic.pretty_to_string (get_resources "books")
-      | _ ->
-          let response = `Assoc [ ("error", `String "There was an error!") ] in
-          Yojson.Basic.pretty_to_string response
-    in
+    let response_json = handle_request uri in
+    let response = Yojson.Basic.pretty_to_string response_json in
     let meth = req |> Request.meth |> Code.string_of_method in
     let headers = req |> Request.headers |> Header.to_string in
-    ( body |> Cohttp_lwt.Body.to_string >|= fun body ->
+  
+    (body |> Cohttp_lwt.Body.to_string >|= fun body ->
       Printf.sprintf "Uri: %s\nMethod: %s\nHeaders\nHeaders: %s\nBody: %s" uri
         meth headers body )
     >>= fun _body -> Server.respond_string ~status:`OK ~body:response ()
   in
+
   let server = Server.make ~callback () in
   let port = 8080 in
   let mode = `TCP (`Port port) in
